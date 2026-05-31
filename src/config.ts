713df715c7dc8cd472767bfb0f065f898055d1fd
guowei_jzzt <3484878603@qq.com>
2026-05-31 16:51:58 +0800

    feat: 新增诗词展示

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 		position: "center", // 等同于 `object-position`，仅支持 `top`、`center`、`bottom`，默认值为 `center`。
 		credit: {
 			enable: true, // 显示横幅图片的署名文字。
-			text: "随拍", // 要显示的署名文字。
+			text: "Bing壁纸", // 要显示的署名文字。
 			url: "", // 可选，原始作品或作者页面的链接。
 		},
 	},
