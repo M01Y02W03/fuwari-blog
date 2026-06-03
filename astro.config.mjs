a44215223034f138949f3bc70108a7884fc49f92
guowei_jzzt <3484878603@qq.com>
2026-06-03 19:35:51 +0800

    perf: 笔记微调

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.awjzzt.cn/",
+	site: "https://jzzt.cc.cd/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
